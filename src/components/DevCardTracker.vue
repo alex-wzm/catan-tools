c910d4ce3332aca13dcacdc0919268d29d660d61
Alex Wong <alex-wzm@outlook.com>
2020-05-26 17:02:59 -0400

    init dev card tracker

---
+++ src/components/DevCardTracker.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h2>Left in deck:</h2>
+    <ul>
+      <li>Knights: {{ cards.knights }}</li>
+      <li>Victory Points: {{ cards.victoryPoints }}</li>
+      <li>Road Building: {{ cards.roadBuilding }}</li>
+      <li>Year of Plenty: {{ cards.yearOfPlenty }}</li>
+      <li>Monopoly: {{ cards.monopoly }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DevCardTracker",
+  data() {
+    return {
+      cards: {
+        knights: 14,
+        victoryPoints: 5,
+        roadBuilding: 2,
+        yearOfPlenty: 2,
+        monopoly: 2
+      }
+    };
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
     <h3>Catan Tools</h3>
+    <dev-card-tracker />
     <div class="footer">
       <github-button
         href="https://github.com/alex-wzm/catan-tools"
         data-icon="octicon-star"
         data-size="large"
         aria-label="Star ntkme/github-buttons on GitHub"
-        >Star</github-button
-      >
+      >Star</github-button>
       <a href="https://vuejs.org/" target="_blank">
         <img height="50px" alt="Vue logo" src="../assets/logo.png" />
       </a>
@@ -18,27 +18,17 @@
 
 <script>
 import GithubButton from "vue-github-button";
+import DevCardTracker from "./DevCardTracker";
 
 export default {
   name: "Home",
-  components: { GithubButton },
+  components: { GithubButton, DevCardTracker }
 };
 </script>
 
 <style scoped>
 h3 {
   margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
 }
 .footer {
   position: absolute;
